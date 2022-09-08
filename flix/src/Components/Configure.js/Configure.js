34693e25f6947f388d29b3c3f7e88604faeb3bb9
Jyoti-Patowary <jpatowary8@gmail.com>
2022-09-09 02:09:40 +0530

    Search Component is not working

---
+++ flix/src/Components/Configure.js/Configure.js
@@ -0,0 +1,4 @@
+
+export const imageSizeSmall= "https://image.tmdb.org/t/p/w300";
+export const imageSizeLarge = "https://image.tmdb.org/t/p/w500";
+export const noContent = "https://www.movienewz.com/img/films/poster-holder.jpg";
